7783a290fd2fd8b5fd13a0e1f700c65db69b74be
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-11-05 09:09:43 +0100

    create and add footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+        <div>
+          <p>Developer: your name</p>
+          <p>
+            Contact us:
+            <a href="mailto:student@notehub.app">student@notehub.app</a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
